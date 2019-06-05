9e07c1d65f1c8c40222a3be68ed9bc9ca4c7cb87
Ralf Westphal <info@ralfw.de>
2019-06-05 15:17:58 +0200

    Added playground project for first steps

---
+++ src/demoapplications/firststeps/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using nsimpleeventstore;
+
+namespace firststeps
+{
+    class NumberEntered : Event
+    {
+        public int Number;
+    }
+
+    class A : Event {}
+    class B : Event {}
+    class C : Event {}
+    
+    
+    // Just a sandbox project to play around
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var es = new InMemoryEventstore();
+
+            es.Record(new A());
+            es.Record(new C());
+            es.Record(new B());
+
+            EventArchive.Write("myarchive.json", es.Replay().Events);
+
+            var events = EventArchive.Read("myarchive.json");
+
+            var es2 = new InMemoryEventstore(events);
+            Console.WriteLine(es2.Replay().Events.Length);
+        }
+    }
+}
