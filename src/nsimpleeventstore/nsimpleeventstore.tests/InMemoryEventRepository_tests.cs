f645dd601c43d8e4ae12cea597e26651feae3db5
Daniel Eggers <mail@danieleggers.de>
2020-12-18 21:28:46 +0100

    removed InMemoryEventStore-tests in favour of InMemoryEventRepository-tests

---
+++ src/nsimpleeventstore/nsimpleeventstore.tests/InMemoryEventRepository_tests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using nsimpleeventstore.adapters.eventrepositories;
+using nsimpleeventstore.contract;
+using Xunit;
+
+namespace nsimpleeventstore.tests
+{
+    public class InMemoryEventRepository_tests
+    {
+        class TestEvent : Event
+        {
+            public string Foo;
+        }
+
+        [Fact]
+        public void No_events_in_empty_repo()
+        {
+            using (var sut = new InMemoryEventRepository(""))
+            {
+                Assert.Equal(0, sut.Count);
+            }
+        }
+
+        [Fact]
+        public void Counting_events()
+        {
+            using (var sut = new InMemoryEventRepository(""))
+            {
+                sut.Store(0, new TestEvent());
+                Assert.Equal(1, sut.Count);
+                sut.Store(1, new TestEvent());
+                Assert.Equal(2, sut.Count);
+            }
+        }
+
+        [Fact]
+        public void Index_must_be_geq_0()
+        {
+            using (var sut = new InMemoryEventRepository(""))
+            {
+                Assert.Throws<InvalidOperationException>(() => sut.Store(-1, new TestEvent()));
+                Assert.Equal(0, sut.Count);
+            }
+        }
+
+        [Fact]
+        public void Store_once()
+        {
+            using (var sut = new InMemoryEventRepository(""))
+            {
+                sut.Store(0, new TestEvent());
+
+                Assert.Throws<InvalidOperationException>(() => sut.Store(0, new TestEvent()));
+                Assert.Equal(1, sut.Count);
+            }
+        }
+
+        [Fact]
+        public void Store_and_load()
+        {
+            using (var sut = new InMemoryEventRepository(""))
+            {
+                var e = new TestEvent { Foo = "Hello" };
+                sut.Store(0, e);
+
+                var result = (TestEvent)sut.Load(0);
+
+                Assert.Equal(e, result);
+                Assert.Equal(e.Id, result.Id);
+                Assert.Equal(e.Foo, result.Foo);
+            }
+        }
+
+    }
+}
